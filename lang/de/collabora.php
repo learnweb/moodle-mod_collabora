7c4f062f025151357e7f9821518f5f07109a95dc
Michael Wuttke <michael.wuttke@beuth-hochschule.de>
2019-04-02 17:48:48 +0200

    added german a language file

---
+++ lang/de/collabora.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package   mod_collabora
+ * @copyright 2019 Davo Smith, Synergy Learning
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cachedef_discovery'] = 'Collabora discovery XML file';
+$string['collabora:addinstance'] = 'Kollaboratives Dokument zu einem Kurs hinzufügen';
+$string['collabora:editlocked'] = 'Gesperrtes kollaboratives Dokument bearbeiten';
+$string['collabora:lock'] = 'Kollaboratives Dokument sperren/entsperren';
+$string['collabora:view'] = 'Kollaboratives Dokument anzeigen';
+$string['collaboraurl'] = 'Collabora URL';
+$string['collaboraurlnotset'] = 'Collabora-URL ist für diese Seite nicht konfiguriert';
+$string['current'] = 'Aktuelle Registerkarte';
+$string['defaultdisplay'] = 'Standardanzeige';
+$string['defaultdisplaydescription'] = 'Standardanzeigebeschreibung';
+$string['defaultdisplayname'] = 'Standardanzeigename';
+$string['defaultformat'] = 'Standardformat';
+$string['display'] = 'Anzeigen';
+$string['displaydescription'] = 'Beschreibung anzeigen';
+$string['displayname'] = 'Name anzeigen';
+$string['eventdocumentlocked'] = 'Kollaboratives Dokument gesperrt';
+$string['eventdocumentunlocked'] = 'Kollaboratives Dokument entsperrt';
+$string['format'] = 'Format';
+$string['height'] = 'Höhe (0 für automatisch)';
+$string['initialfile'] = 'Ausgangsdatei';
+$string['initialtext'] = 'Ausgangstext';
+$string['locked'] = 'Dokument von Lehrkraft gesperrt';
+$string['lockedunlock'] = 'Das Dokument ist derzeit gesperrt. Klicken Sie hier, um die Sperre aufzuheben.';
+$string['modulename'] = 'Kollaboratives Dokument';
+$string['modulenameplural'] = 'Kollaborative Dokumente';
+$string['name'] = 'Name';
+$string['new'] = 'Neue Registrierkarte';
+$string['nogroupaccess'] = 'Diese Aktivität steht nur Benutzern zur Verfügung, die Mitglieder einer Kursgruppe sind';
+$string['pluginadministration'] = 'Einstellungen für kollaboratives Dokument';
+$string['pluginname'] = 'Kollaboratives Dokument';
+$string['presentation'] = 'Presentation';
+$string['requiredfortext'] = 'Erforderlich, wenn das Format \'Specified text\' ist';
+$string['requiredforupload'] = 'Erforderlich, wenn das Format \'File upload\' ist';
+$string['spreadsheet'] = 'Tabellen-Dokument';
+$string['text'] = 'Spezifischer Text';
+$string['unsupportedtype'] = 'Nicht unterstützter Dateityp {$a}';
+$string['upload'] = 'Datei hochladen';
+$string['unlockedlock'] = 'Dokument ist derzeit nicht gesperrt. Klicken Sie hier, um es zu sperren und die Bearbeitung zu verhindern.';
+$string['width'] = 'Breite (0 für automatisch)';
+$string['wordprocessor'] = 'Word-Dokument';
