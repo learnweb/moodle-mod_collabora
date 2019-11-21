7e772a712d4bbe52dcfce1911e9e7bb4cb555fdc
Justus Dieckmann <justusdieckmann@wwu.de>
2019-11-21 10:36:15 +0100

    Edit comments

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -69,7 +69,7 @@ class provider implements
      * @return  contextlist   $contextlist  The contextlist containing the list of contexts used in this plugin.
      */
     public static function get_contexts_for_userid(int $userid): contextlist {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
         return new contextlist();
     }
 
@@ -79,7 +79,7 @@ class provider implements
      * @param approved_contextlist $contextlist The approved contexts to export information for.
      */
     public static function export_user_data(approved_contextlist $contextlist) {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
     }
 
     /**
@@ -88,7 +88,7 @@ class provider implements
      * @param \context $context The specific context to delete data for.
      */
     public static function delete_data_for_all_users_in_context(\context $context) {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
     }
 
     /**
@@ -97,7 +97,7 @@ class provider implements
      * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
      */
     public static function delete_data_for_user(approved_contextlist $contextlist) {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
     }
 
     /**
@@ -106,7 +106,7 @@ class provider implements
      * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
      */
     public static function get_users_in_context(userlist $userlist) {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
     }
 
     /**
@@ -115,6 +115,6 @@ class provider implements
      * @param approved_userlist $userlist The approved context and user information to delete information for.
      */
     public static function delete_data_for_users(approved_userlist $userlist) {
-        // We don't delete files, because they was submitted as a group. To avoid confusion, we don't export them either.
+        // We don't delete files, because they were submitted as a group. To avoid confusion, we don't export them either.
     }
 }
