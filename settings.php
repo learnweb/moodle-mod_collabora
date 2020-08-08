7afd352dde6dc48d1312aa893450c90079873399
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-08-08 19:43:40 -0400

    Dynamic server branding support
    The Online server product name is now
    dynamically pulled from the server, once a
    valid URL is provided. To avoids using
    a hard-coded name. After the initial
    discovery of the server capabilities, the
    product-name is cached.
    
    However we allow for changing the server
    (or, upon updating the server the product
    name may have been changed) by clearing
    the cache from the plugin settings page.
    Simply loading the settings page will
    invalidate the cache, if a URL is already
    set and is valid, and will pull the new
    name.
    
    The relevant strings displayed in the UI,
    including errors, have been updated to use
    the product name provided by the server
    with proper fallback.

--- settings.php
+++ settings.php
@@ -25,8 +25,35 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
+
+    $productname = '';
+    try {
+        // If we have a URL, fetch the product name from the server.
+        // And if we had it, re-fetch it, in case it's changed.
+        if (get_config('mod_collabora', 'url')) {
+            $collabora = new \mod_collabora\collabora(0, 0, -1, 0);
+            // Remove from the cache to force fetching.
+            $collabora->reset_caches();
+            $productname = $collabora->get_product_name();
+        }
+    } catch (Exception $e) {
+        // For some reason we couldn't discover the server name.
+        // Possibly the server doesn't have the capabilities file.
+        $productname = '';
+    }
+    finally {
+        // Fallback.
+        if (empty($productname)) {
+            $cache = \cache::make('mod_collabora', 'capabilities');
+            if (!$productname = $cache->get('productname')) {
+                // If it's not already cached, default, as we are aren't configured yet.
+                $productname = get_string('default_server_name', 'mod_collabora');
+            }
+        }
+    }
+
     $settings->add(new admin_setting_configtext('mod_collabora/url',
-                                                new lang_string('collaboraurl', 'mod_collabora'), '', '', PARAM_URL));
+                                                new lang_string('collaboraurl', 'mod_collabora', $productname), '', '', PARAM_URL));
 
     $settings->add(new admin_setting_configselect('mod_collabora/defaultformat',
                                                   new lang_string('defaultformat', 'mod_collabora'), '',
