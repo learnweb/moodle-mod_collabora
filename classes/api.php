fcc5cfd324badbc3a413e305b67516962dac0a83
Johannes Burk <jojoob@users.noreply.github.com>
2022-04-04 10:21:03 +0200

    Ensure LastModifiedTime is in UTC
    Collabora API requires LastModifiedTime in ISO 8601 UTC format ('Y-m-d\TH:i:s.u\Z')
    see https://github.com/EGroupware/collabora/blob/06bc8ea996f9c54caa1e5205da7842b5af186c91/src/Wopi/Files.php#L31

--- classes/api.php
+++ classes/api.php
@@ -230,6 +230,8 @@ class api {
      * Handle checkfileinfo requests.
      */
     private function handle_checkfileinfo() {
+        $tz = date_default_timezone_get();
+        date_default_timezone_set('UTC');
         $file = $this->get_stored_file();
         $ret = (object)[
             'BaseFileName' => clean_filename($file->get_filename()),
@@ -241,6 +243,7 @@ class api {
             'UserCanNotWriteRelative' => true,
             'LastModifiedTime' => date('c', $file->get_timemodified()),
         ];
+        date_default_timezone_set($tz);
         die(json_encode($ret));
     }
 }
