c952394b60b9f9d41ade58d69c49976f5850beaa
Andreas Grabs <moodle@grabs-edv.de>
2022-04-05 12:51:20 +0200

    grabs-edv mod_collabora: increase version and update CHANGES

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021102602;
+$plugin->version = 2021102603;
 $plugin->requires = 2020061501; // Moodle 3.9.
 $plugin->component = 'mod_collabora';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.9-r10 (2022-03-06)';
+$plugin->release = 'v3.9-r11 (2022-04-04)';
